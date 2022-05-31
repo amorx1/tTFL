d9784ac28649fda7ae0758a91ce9e8c1e8a36086
amorx1 <akshay.morx@gmail.com>
2022-05-31 18:22:47 +0100

    layout complete

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,9 @@
 use std::io;
 
 use crossterm::event::{self, Event, KeyCode};
+use serde_derive::{Serialize, Deserialize};
 use tui::{backend::Backend, Terminal};
+use reqwest::{self, Client};
 
 use crate::ui::ui;
 
@@ -12,13 +14,43 @@ pub enum InputMode {
 
 pub enum Focus {
     InputBlock,
+    LinesBlock
 }
 
 pub struct App {
     pub input: String,
     pub input_mode: InputMode,
     pub messages: Vec<String>,
+    pub lineNames: Vec<String>,
     pub focus: Option<Focus>,
+    pub line_selected: Option<usize>,
+    pub lines_tree_size: Option<usize>,
+}
+#[derive(Debug, Serialize, Deserialize)]
+pub struct LineStatus {
+    id: i32,
+    statusSeverity: i32,
+    statusSeverityDescription: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Disruption {
+    category: String,
+    categoryDescription: String,
+    description: String,
+    summary: String,
+    additionalInfo: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Line {
+    id: String,
+    name: String,
+    modeName: String,
+    disruptions: Vec<Disruption>,
+    lineStatuses: Vec<LineStatus>,
+    // routeSections: Vec<String>,
+    // serviceTypes: Vec<ServiceType>,
 }
 
 impl Default for App {
@@ -27,13 +59,97 @@ impl Default for App {
             input: String::new(),
             input_mode: InputMode::Normal,
             messages: Vec::new(),
+            lineNames: Vec::new(),
             focus: None,
+            line_selected: Some(0),
+            lines_tree_size: Some(0)
         }
     }
 }
 
-pub fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<()> {
+#[tokio::main]
+pub async fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<()> {
+    // load data once here before loop
+    app.lineNames = reqwest::get("https://api.tfl.gov.uk/line/mode/tube/status").await
+        .unwrap()
+        .json::<Vec<Line>>()
+        .await
+        .unwrap()
+        .iter()
+        .map(|i| String::from(&i.name))
+        .collect::<Vec<_>>();
+    // begin loop
     loop {
         terminal.draw(|f| ui(f, &mut app))?;
+
+        if let Event::Key(key) = event::read()? {
+            match app.input_mode {
+                InputMode::Normal => match key.code {
+                    KeyCode::Char('i') => {
+                        app.input_mode = InputMode::Insert;
+                        app.focus = Some(Focus::InputBlock);
+                    }
+                    KeyCode::Char('l') => {
+                        app.focus = Some(Focus::LinesBlock);
+                    }
+                    KeyCode::Char('q') => {
+                        return Ok(());
+                    }
+                    KeyCode::Char('r') => {
+                        // refresh all data here manually
+                        let result = reqwest::get("https://api.tfl.gov.uk/line/mode/tube/status").await.unwrap().json::<Vec<Line>>().await.unwrap();
+                        let names = result.iter().map(|i| String::from(&i.name)).collect::<Vec<_>>();
+                        app.lineNames = names;
+                    }
+                    KeyCode::Esc => {
+                        app.focus = None;
+                    }
+                    KeyCode::Char('j') => match app.focus {
+                        Some(Focus::LinesBlock) => {
+                            if app.lines_tree_size
+                                > usize::checked_add(
+                                    app.line_selected.unwrap(),
+                                    usize::try_from(1).unwrap(),
+                                )
+                            {
+                                app.line_selected = usize::checked_add(
+                                    app.line_selected.unwrap(),
+                                    usize::try_from(1).unwrap(),
+                                );
+                            }
+                        }
+                        _ => {}
+                    },
+                    KeyCode::Char('k') => match app.focus {
+                        Some(Focus::LinesBlock) => {
+                            if app.line_selected != Some(0) {
+                                app.line_selected = usize::checked_sub(
+                                    app.line_selected.unwrap(),
+                                    usize::try_from(1).unwrap(),
+                                );
+                            }
+                        }
+                        _ => {}
+                    },
+                    _ => {}
+                },
+                InputMode::Insert => match key.code {
+                    KeyCode::Enter => {
+                        app.messages.push(app.input.drain(..).collect());
+                    }
+                    KeyCode::Char(c) => {
+                        app.input.push(c);
+                    }
+                    KeyCode::Backspace => {
+                        app.input.pop();
+                    }
+                    KeyCode::Esc => {
+                        app.input_mode = InputMode::Normal;
+                        app.focus = None;
+                    }
+                    _ => {}
+                },
+            }
+        }
     }
 }
--- src/ui.rs
+++ src/ui.rs
@@ -23,6 +23,9 @@ pub fn ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
             .constraints([Constraint::Percentage(30), Constraint::Percentage(80)].as_ref())
             .direction(Direction::Horizontal)
             .split(chunks[1]);
+
+        draw_messages(f, app, chunks[0]);
+        draw_preview(f, app, chunks[1]);
     }
 }
 
@@ -63,4 +66,90 @@ fn draw_input<B: Backend>(f: &mut Frame<B>, app: &App, area: Rect) {
         InputMode::Normal => {}
         InputMode::Insert => f.set_cursor(area.x + app.input.width() as u16 + 1, area.y + 1),
     }
+}
+
+fn draw_messages<B: Backend>(f: &mut Frame<B>, app: &mut App, area: Rect) {
+    let chunks = Layout::default()
+        .constraints([Constraint::Percentage(100)])
+        .direction(Direction::Vertical)
+        .split(area);
+
+    {
+        let chunks = Layout::default()
+            .constraints([Constraint::Percentage(100)])
+            .direction(Direction::Vertical)
+            .split(chunks[0]);
+
+        let items = app.lineNames.iter().map(String::from).map(ListItem::new).collect::<Vec<_>>();
+        app.lines_tree_size = Some(items.len());
+
+        let lines = List::new(items)
+            .block(
+                Block::default()
+                    .title("Lines")
+                    .title_alignment(Alignment::Center)
+                    .borders(Borders::ALL)
+                    .border_type(BorderType::Rounded),
+            )
+            .style(match app.focus {
+                Some(Focus::LinesBlock) => Style::default().fg(Color::Red),
+                _ => Style::default().fg(Color::White),
+            })
+            .highlight_style(Style::default().add_modifier(Modifier::ITALIC))
+            .highlight_symbol(" ");
+
+        let mut state = ListState::default();
+        state.select(app.line_selected);
+        f.render_stateful_widget(lines, area, &mut state)
+    }
+}
+
+fn draw_preview<B: Backend>(f: &mut Frame<B>, app: &App, area: Rect) {
+    let chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([Constraint::Percentage(100)])
+        .split(area);
+
+    let block = Block::default()
+        .title("Preview")
+        .title_alignment(Alignment::Center)
+        .borders(Borders::ALL)
+        .border_style(Style::default().fg(Color::White))
+        .border_type(BorderType::Rounded)
+        .style(Style::default());
+    f.render_widget(block, area);
+
+    let chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .margin(4)
+        .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
+        .split(area);
+
+    // Top two inner blocks
+    let top_chunks = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
+        .split(chunks[0]);
+
+    // Top left inner block with green background
+    let block = Block::default().title("With borders").borders(Borders::ALL);
+    f.render_widget(block, top_chunks[0]);
+
+    // Top right inner block with styled title aligned to the right
+    let block = Block::default().title("With borders").borders(Borders::ALL);
+    f.render_widget(block, top_chunks[1]);
+
+    // Bottom two inner blocks
+    let bottom_chunks = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
+        .split(chunks[1]);
+
+    // Bottom left block with all default borders
+    let block = Block::default().title("With borders").borders(Borders::ALL);
+    f.render_widget(block, bottom_chunks[0]);
+
+    // Bottom right block with styled left and right border
+    let block = Block::default().title("With borders").borders(Borders::ALL);
+    f.render_widget(block, bottom_chunks[1]);
 }
